aa4aea559e0b58596b51fd3b1da0d864d65dd677
Todor <84274078+TLizhev@users.noreply.github.com>
2021-11-15 10:51:05 +0000

    Add project files.

---
+++ CodeWarsSolvedKatas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeWarsSolvedKatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
