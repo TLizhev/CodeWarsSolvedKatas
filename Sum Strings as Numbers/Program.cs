49d6df8d5015d08dfe687350dc555e9b98a7d145
Todor <84274078+TLizhev@users.noreply.github.com>
2021-12-15 11:18:16 +0000

    Solve SumStringsAsNumbers kata

---
+++ Sum Strings as Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+public static class Kata
+{
+    public static string sumStrings(string a, string b)
+    {
+        if (a == string.Empty)
+        {
+            return BigInteger.Parse(b).ToString();
+        }
+        if (b == string.Empty)
+        {
+            return BigInteger.Parse(a).ToString();
+        }
+        return (BigInteger.Parse(a) + BigInteger.Parse(b)).ToString();
+
+    }
+}
