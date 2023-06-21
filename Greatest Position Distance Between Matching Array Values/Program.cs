5eda68b61723ca32c2f596ffcd527d072e1edd12
Todor <84274078+TLizhev@users.noreply.github.com>
2023-06-21 21:34:19 +0100

    Add Kata

---
+++ Greatest Position Distance Between Matching Array Values/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Greatest_Position_Distance_Between_Matching_Array_Values
+{
+    public class GreatestDistance
+    {
+        public static int Exec(List<int> data)
+        {
+            var list = new List<int>();
+            var max = 0;
+            var duplicates = data.GroupBy(x => x)
+                .Where(g => g.Count() > 1)
+                .Select(y => new { Item = y.Key, Count = y.Count() })
+                .ToList();
+
+            if (duplicates.Count == 0)
+            {
+                return 0;
+            }
+
+            foreach (var duplicate in duplicates)
+            {
+                for (int i = 0; i < data.Count; i++)
+                {
+                    if (data[i] == duplicate.Item)
+                    {
+                        list.Add(i);
+                    }
+                }
+                var current = list.Last() - list.First();
+                if (current > max)
+                {
+                    max = current;
+                }
+                list = new List<int>();
+            }
+
+            return max;
+        }
+    }
+}
