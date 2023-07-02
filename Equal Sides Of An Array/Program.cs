9809956aefd69e2e001bc4099d900143698b389d
Todor <84274078+TLizhev@users.noreply.github.com>
2023-07-02 18:46:36 +0100

    Push solved Kata

---
+++ Equal Sides Of An Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System;
+
+public class Kata
+{
+    public static int FindEvenIndex(int[] arr)
+    {
+        if (arr.Length == 0)
+            return 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            var left = arr.Take(i).Sum(x => x);
+            var right = arr.TakeLast(arr.Length - i).Sum(x => x) - arr[i];
+            if (left == right)
+            {
+                return i;
+            }
+            left = right = 0;
+        }
+
+        return -1;
+    }
+}
