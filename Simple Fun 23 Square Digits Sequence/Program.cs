a991c3c7303d035b8368edd97073d3c770b55530
Todor <84274078+TLizhev@users.noreply.github.com>
2023-07-02 21:19:26 +0100

    Push solved kata

---
+++ Simple Fun 23 Square Digits Sequence/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Simple_Fun_23_Square_Digits_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.SquareDigitsSequence(1));
+        }
+    }
+
+    public class Kata
+    {
+        public static int SquareDigitsSequence(int a0)
+        {
+            var collection = new List<int> { a0, Sequence(a0) };
+            if (collection.First() == collection.Last())
+            {
+                return collection.Count;
+            }
+
+            while (true)
+            {
+                var item = Sequence(collection.Last());
+
+                if (collection.Contains(item))
+                {
+                    return collection.Count + 1;
+                }
+                collection.Add(item);
+            }
+        }
+
+        public static int Sequence(int number)
+        {
+            return (int)number.ToString()
+                 .Select(char.GetNumericValue)
+                 .Select(x => Math.Pow(x, 2))
+                 .Sum();
+        }
+    }
+}
